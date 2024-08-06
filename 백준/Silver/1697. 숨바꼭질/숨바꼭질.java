79a7cacc119695f10f6528533c0eed70044146db
KimEuiSoo <63793652+KimEuiSoo@users.noreply.github.com>
2024-08-06 12:54:44 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 18636 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.lang.*;
+import java.util.*;
 
 public class Main {
-    static int n = 0, k = 0, res=0;
-    static int[] visited = new int[100001];
+    static int N = 0, K = 0, res=0;
+    static int[] visited= new int[100001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        bfs(n);
+        bfs(N);
         System.out.println(res);
     }
 
-    public static void bfs(int node){
+    public static void bfs(int x){
         Queue<Integer> qu = new LinkedList<Integer>();
 
-        qu.add(node);
-        int nodeN = 0;
-        visited[node] = 1;
+        qu.add(x);
+        int dx = 0;
+        visited[x] = 1;
         while(qu.isEmpty()==false){
-            nodeN = qu.remove();
-            if(nodeN==k){
-                res=visited[nodeN]-1;
+            dx = qu.remove();
+            if(dx==K){
+                res=visited[dx]-1;
             }
-            if(nodeN-1>=0 && visited[nodeN-1]==0){
-                visited[nodeN-1] = visited[nodeN]+1;
-                qu.add(nodeN-1);
+            if(dx-1>=0 && visited[dx-1]==0){
+                visited[dx-1] = visited[dx]+1;
+                qu.add(dx-1);
             }
-            if(nodeN+1<=100000 && visited[nodeN+1]==0){
-                visited[nodeN+1] = visited[nodeN]+1;
-                qu.add(nodeN+1);
+            if(dx+1<=100000 && visited[dx+1]==0){
+                visited[dx+1] = visited[dx]+1;
+                qu.add(dx+1);
             }
-            if(nodeN*2<=100000 && visited[nodeN*2]==0){
-                visited[nodeN*2] = visited[nodeN]+1;
-                qu.add(nodeN*2);
+            if(dx*2<=100000 && visited[dx*2]==0){
+                visited[dx*2] = visited[dx]+1;
+                qu.add(dx*2);
             }
         }
     }
