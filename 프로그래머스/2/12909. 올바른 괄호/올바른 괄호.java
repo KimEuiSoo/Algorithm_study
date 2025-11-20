fff287c172cd5e278f03724d7ff05c38cce6d4c5
KimEuiSoo <63793652+KimEuiSoo@users.noreply.github.com>
2025-11-20 21:09:29 +0900

    [level 2] Title: 올바른 괄호, Time: 16.71 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,32 +1,18 @@
 import java.util.*;
-import java.io.*;
+import java.lang.*;
 
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        
-        Stack<String> st = new Stack<>();
-        
-        for(int i=1; i<=s.length(); i++){
-            char text = s.charAt(i-1);
-            if(i==1){
-                st.push(String.valueOf(text));
-            }
-            else if(text == (')')){
-                if(st.isEmpty()){
-                    return false;
-                }
-                st.pop();
-            }else{
-                st.push(String.valueOf(text));
+        Stack<Character> stack = new Stack();
+        for(char c : s.toCharArray()){
+            if(c=='(') stack.push(c);
+            else{
+                if(stack.isEmpty()) return false;
+                stack.pop();
             }
         }
-        
-        if(!st.isEmpty()){
-            answer = false;
-        }
-        
 
-        return answer;
+        return stack.isEmpty();
     }
 }
